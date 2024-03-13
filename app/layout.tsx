ca5e779ed73b07131b6853e9d70d2c6f00e5daa0
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2024-03-13 13:30:22 +0530

    [FEAT] - React Query  : Added react query and created the provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Open_Sans } from "next/font/google";
 import "./globals.css";
+import Provider from "@/utils/Providers";
 
 const inter = Open_Sans({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + "font-fa"}>{children}</body>
+      <body className={inter.className + "font-fa"}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
--- services/dataservice.ts
+++ services/dataservice.ts
@@ -5,34 +5,26 @@ interface DataStoreState {
   data: any | null;
   loading: boolean;
   error: string | null;
-  fetchData: (url: string) => Promise<void>;
-  postData: (url: string, data: any) => Promise<void>;
+  // fetchData: (url: string) => Promise<void>;
 }
 
 const useDataStore = create<DataStoreState>((set) => ({
-  data: null,
+  data: {
+    userName: "Tharindu",
+  },
   loading: false,
   error: null,
 
-  fetchData: async (url: string) => {
-    set({ loading: true, error: null });
-    try {
-      const response: AxiosResponse<any> = await axios.get(url);
-      set({ data: response.data, loading: false });
-    } catch (error: any) {
-      set({ error: error.message, loading: false });
-    }
-  },
+  // fetchData: async (url: string) => {
+  //   set({ loading: true, error: null });
+  //   try {
+  //     const response: AxiosResponse<any> = await axios.get(url);
+  //     set({ data: response.data, loading: false });
+  //   } catch (error: any) {
+  //     set({ error: error.message, loading: false });
+  //   }
+  // },
 
-  postData: async (url: string, data: any) => {
-    set({ loading: true, error: null });
-    try {
-      const response: AxiosResponse<any> = await axios.post(url, data);
-      set({ data: response.data, loading: false });
-    } catch (error: any) {
-      set({ error: error.message, loading: false });
-    }
-  },
 }));
 
 export default useDataStore;
